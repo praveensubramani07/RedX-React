01efd72aca6fc835743d195116bec3521208d247
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2024-01-09 04:03:00 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,18 @@ import React, { useState, useEffect } from 'react';
 import NavBar from './Navbar';
 import Post from './Post';
 import { Cookies } from 'react-cookie';
+import  url from './config';
 
 export default function Home() {
   const cookie=new Cookies();
   const userId=cookie.get('userEmail')
   const [dataSets, getDataSets] = useState([]);
-  const url = `http://localhost:4000/api/feed/${userId}`;
+  const url1 = `${url}feed/${userId}`;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url1);
         const allData = await response.json();
         getDataSets(allData);
       } catch (error) {
