db882cf4e45cefa950e0970b82888dd4885c5142
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2024-01-09 03:59:45 -0800

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 // config.js
-//const url = 'https://long-gray-eel-sock.cyclic.app/api/';
-const url='http://localhost:4000/api/'
+const url = 'https://long-gray-eel-sock.cyclic.app/api/';
+//const url='http://localhost:4000/api/'
 export default url;
