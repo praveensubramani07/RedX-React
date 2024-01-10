13843ed72c850a0447ffa11c110946037532a3a0
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2024-01-09 20:34:41 -0800

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,5 @@
 // config.js
-const url = 'https://long-gray-eel-sock.cyclic.app/api/';
+//const url = 'https://long-gray-eel-sock.cyclic.app/api/';
 //const url='http://localhost:4000/api/'
+const url='https://redx-project.onrender.com/api/';
 export default url;
