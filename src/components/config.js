2fc83ef0e4eb9862199894b587f4993fcd8b1beb
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2024-03-21 19:01:12 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 // config.js
 //const url = 'https://long-gray-eel-sock.cyclic.app/api/';
 //const url='http://localhost:4000/api/'
-const url='https://thankful-erin-cape-buffalo.cyclic.app/api/';
+const url='https://node-v5uf.onrender.com/api/';
 export default url;
