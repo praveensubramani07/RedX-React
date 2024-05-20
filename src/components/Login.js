2ff7fe41e09db130d830092d1dadf2bb30deea3b
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2024-05-20 18:04:50 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -70,7 +70,7 @@ function Login() {
     <>
       {!userEmail && (
         <div className="google">
-          <p className="login-p">Login with google</p>
+          <p className="login-p">This projects api is not running currently, please explore other projects of me</p>
           <GoogleLogin
             onSuccess={(credentialResponse) => {
               try {
